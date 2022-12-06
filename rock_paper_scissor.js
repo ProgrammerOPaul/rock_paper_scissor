706693898875cbab9c66d91dbd304cc6d45ab28e
Paul Ortiz <paulortiz10@outlook.com>
2022-12-06 11:41:14 -0400

    Added the round system, where we determine a winner

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,10 +1,12 @@
 // This is the list of possible outcomes
 
-let input = prompt("Type 0 for Rock, 1 for Paper and 2 for Scissors");
-
-console.log(choice[input]);
-
 const choice = ["Rock","Paper", "Scissors"];
+
+// This is the user input
+let input = prompt("Type 0 for Rock, 1 for Paper and 2 for Scissors");
+let userChoice = choice[input]
+
+console.log(userChoice);
 
 
 // This returns a choice for the computer
@@ -12,3 +14,31 @@ function getComputerChoice(choice){
     move = Math.floor(Math.random() * 3)
     return choice [move]
 }
+
+console.log(getComputerChoice(choice))
+
+function round(user,computer){
+    if (user == "Rock" && computer == "Scissors" ){
+        console.log("You win! Rock beats Scissors")
+    }
+    else if (user == "Rock" && computer == "Paper" ){
+        console.log("You lose! Paper beats Rock")
+    }
+    else if (user == "Paper" && computer == "Rock" ){
+        console.log("You win! Paper beats Rock")
+    }
+    else if (user == "Paper" && computer == "Scissors" ){
+        console.log("You lose! Scissors beats Paper")
+    }
+    else if (user == "Scissors" && computer == "Paper" ){
+        console.log("You win! Scissors beats Paper")
+    }
+    else if (user == "Scissors" && computer == "Rock" ){
+        console.log("You lose! Rock beats Scissors")
+    }
+    else{
+        console.log(`It's a tie, you both chose ${user}`)
+    }
+}
+
+round (userChoice,getComputerChoice(choice))
