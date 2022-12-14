32f1c79d6db622f69fb6bceebf57970fe93515d1
Paul Ortiz <paulortiz10@outlook.com>
2022-12-14 11:10:33 -0400

    added CSS file,and some light GUI elements in html

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -4,13 +4,6 @@ const choice = ["Rock","Paper", "Scissors"];
 
 let roundWinner 
 
-// This is the user input
-function userChoice(choice) {
-    let input = prompt("Type 0 for Rock, 1 for Paper and 2 for Scissors")
-    let user
-    return user = choice[input]
-}
-
 // This returns a choice for the computer
 function getComputerChoice(choice){
     move = Math.floor(Math.random() * 3)
@@ -51,30 +44,33 @@ function round(user,computer){
     }
 }
 
-function game(){
-    let userScore = 0
-    let computerScore = 0
-    for (let i = 0; i < 5; i ++) {
-        round(userChoice(choice),getComputerChoice(choice))
-        if (roundWinner == 1){
-            userScore ++
-        }
-        else if (roundWinner == 0){
-            computerScore ++
-        }
-        console.log(`The score is: You with ${userScore} points VS computer with ${computerScore} points`)
-    }
-    console.log("Rounds are over, time to check final score!")
-    if (userScore > computerScore) {
-        return console.log (`You won with ${userScore} points against computer with ${computerScore} points`)
-    }
-    else if (userScore < computerScore) {
-        return console.log (`Computer won with ${computerScore} points against you with ${userScore} points`)
-    }
-    else {
-        return console.log (`You both tie with ${computerScore} points`)
-    }
-}
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", round("rock", getComputerChoice(choice)));
+
+// function game(){
+//     let userScore = 0
+//     let computerScore = 0
+//     for (let i = 0; i < 5; i ++) {
+//         round(userChoice(choice),getComputerChoice(choice))
+//         if (roundWinner == 1){
+//             userScore ++
+//         }
+//         else if (roundWinner == 0){
+//             computerScore ++
+//         }
+//         console.log(`The score is: You with ${userScore} points VS computer with ${computerScore} points`)
+//     }
+//     console.log("Rounds are over, time to check final score!")
+//     if (userScore > computerScore) {
+//         return console.log (`You won with ${userScore} points against computer with ${computerScore} points`)
+//     }
+//     else if (userScore < computerScore) {
+//         return console.log (`Computer won with ${computerScore} points against you with ${userScore} points`)
+//     }
+//     else {
+//         return console.log (`You both tie with ${computerScore} points`)
+//     }
+// }
 
 
 game()
