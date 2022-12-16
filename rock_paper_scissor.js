693f4c9b4f04bdd657d8192387348e5c3453669d
Paul Ortiz <paulortiz10@outlook.com>
2022-12-16 11:22:44 -0400

    Added functionality to buttons

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -15,19 +15,15 @@ function getComputerChoice(choice){
 // This compares userinput vs computer input and returns the winner
 function round(user,computer){
     if (user == "Rock" && computer == "Scissors" ){
-        console.log("You win the round! Rock beats Scissors")
         return roundWinner = 1
     }
     else if (user == "Rock" && computer == "Paper" ){
-        console.log("You lose the round! Paper beats Rock")
         return roundWinner = 0
     }
     else if (user == "Paper" && computer == "Rock" ){
-        console.log("You win the round! Paper beats Rock")
         return roundWinner = 1
     }
     else if (user == "Paper" && computer == "Scissors" ){
-        console.log("You lose the round! Scissors beats Paper")
         return roundWinner = 0
     }
     else if (user == "Scissors" && computer == "Paper" ){
@@ -39,38 +35,68 @@ function round(user,computer){
         return roundWinner = 0
     }
     else{
-        console.log(`It's a tie, you both chose ${user} and get 0 points`)
         return roundWinner = 2
     }
 }
 
-const rock = document.querySelector("#rock");
-rock.addEventListener("click", round("rock", getComputerChoice(choice)));
-
-// function game(){
-//     let userScore = 0
-//     let computerScore = 0
-//     for (let i = 0; i < 5; i ++) {
-//         round(userChoice(choice),getComputerChoice(choice))
-//         if (roundWinner == 1){
-//             userScore ++
-//         }
-//         else if (roundWinner == 0){
-//             computerScore ++
-//         }
-//         console.log(`The score is: You with ${userScore} points VS computer with ${computerScore} points`)
-//     }
-//     console.log("Rounds are over, time to check final score!")
-//     if (userScore > computerScore) {
-//         return console.log (`You won with ${userScore} points against computer with ${computerScore} points`)
-//     }
-//     else if (userScore < computerScore) {
-//         return console.log (`Computer won with ${computerScore} points against you with ${userScore} points`)
-//     }
-//     else {
-//         return console.log (`You both tie with ${computerScore} points`)
-//     }
-// }
+let userScore = 0;
+let computerScore = 0;
+let matches = 0;
+let result = ''
 
 
-game()
+const rock = document.getElementById("rock");
+rock.addEventListener("click", function(){
+    if(round("Rock",(getComputerChoice(choice))) == 0 ){
+        computerScore += 1;
+        result = 'You lose the round! Paper beats Rock';
+    }
+    else if (round("Rock",(getComputerChoice(choice))) == 1){
+        userScore += 1;
+        result = 'You win the round! Rock beats Scissors';
+    }
+    else if (round("Rock",(getComputerChoice(choice))) == 2){
+        result = `It's a tie, you both chose Rock and get 0 points`
+    }
+    matches += 1;
+});
+
+const paper = document.getElementById("paper");
+paper.addEventListener("click", function(){
+    if(round("Paper",(getComputerChoice(choice))) == 0 ){
+        computerScore += 1;
+        result = 'You lose the round! Scissors beats Paper';
+    }
+    else if (round("Paper",(getComputerChoice(choice))) == 1){
+        userScore += 1;
+        result = 'You win the round! Paper beats Rock';
+    }
+
+    else if (round("Paper",(getComputerChoice(choice))) == 2){
+        result = `It's a tie, you both chose Paper and get 0 points`
+    }
+    matches += 1;
+});
+
+const scissors = document.getElementById("scissors");
+scissors.addEventListener("click", function(){
+    if(round("Scissors",(getComputerChoice(choice))) == 0 ){
+        computerScore += 1;
+        result = 'You lose the round! Rock beats Scissors';
+    }
+    else if (round("Scissors",(getComputerChoice(choice))) == 1){
+        userScore += 1;
+        result = 'You win the round! Scissors beats Paper';
+    }
+
+    else if (round("Paper",(getComputerChoice(choice))) == 2){
+        result = `It's a tie, you both chose Scissors and get 0 points`
+    }
+    matches += 1;
+});
+
+const text = document.querySelector('#text');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent(`Player: ${userScore}     VS      Computer: ${computerScore}`);
+text.appendChild(content);
